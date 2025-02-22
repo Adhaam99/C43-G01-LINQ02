e0ea9ba9e6f91ac440309a76357696040907f1ad
adham yehia <adhamyehia194@gmail.com>
2025-02-22 22:18:46 +0200

    Grouping Operators update

---
+++ Assignment/WordComparer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class WordComparer : IEqualityComparer<string>
+    {
+        public bool Equals(string? x, string? y)
+        {
+            if (x == null || y == null) 
+                return false;
+
+            string X = string.Concat(x.OrderBy(C => C));
+            string Y = string.Concat(y.OrderBy(C => C));
+
+            return X == Y;
+
+        }
+
+        public int GetHashCode([DisallowNull] string obj)
+        {
+            return string.Concat(obj.OrderBy(C => C)).GetHashCode();
+        }
+    }
+}
